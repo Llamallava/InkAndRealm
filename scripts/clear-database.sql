29ba03738572c318ed2f6d7b69c1707458675ac2
Matthew <matthew.eberhart@oit.edu>
2026-01-14 12:18:49 -0800

    Added SQL script for purging the database to a clean slate

---
+++ scripts/clear-database.sql
@@ -0,0 +1,21 @@
+﻿-- Clear all data for InkAndRealm (SQL Server)
+-- Order respects FK constraints. No schema changes.
+
+SET NOCOUNT ON;
+BEGIN TRANSACTION;
+
+DELETE FROM dbo.FeatureRelationships;
+DELETE FROM dbo.TownStructures;
+DELETE FROM dbo.FeaturePoints;
+DELETE FROM dbo.Features;
+DELETE FROM dbo.Maps;
+DELETE FROM dbo.Users;
+
+DBCC CHECKIDENT ('dbo.FeatureRelationships', RESEED, 0);
+DBCC CHECKIDENT ('dbo.TownStructures', RESEED, 0);
+DBCC CHECKIDENT ('dbo.FeaturePoints', RESEED, 0);
+DBCC CHECKIDENT ('dbo.Features', RESEED, 0);
+DBCC CHECKIDENT ('dbo.Maps', RESEED, 0);
+DBCC CHECKIDENT ('dbo.Users', RESEED, 0);
+
+COMMIT TRANSACTION;
